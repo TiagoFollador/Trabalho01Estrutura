1e4ccb6b3a7370a81a06ffda43482d91eaea6342
TiagoFollador <tiagobritofollador@gmail.com>
2024-09-04 19:39:43 -0300

    Comentei o codigo para nos auxiliar na apresentacao

--- Pilha/PilhaController.java
+++ Pilha/PilhaController.java
@@ -4,26 +4,28 @@ public class PilhaController {
     public PilhaController(){
         this.pilha = new Pilha();
     }
-    private void inserirDadoPilha(Node dado){
+    private void inserirDadoPilha(Node dado){ // metodo que realiza a insercao na pilha
         dado.setAnterior(pilha.getTopo());
         pilha.setTopo(dado);
     }
 
-    public void createNode(Elemento elemento){
+    public void createNode(Elemento elemento){ // metodo que cria o node (Nó)
         Node node = new Node(elemento);
         inserirDadoPilha(node);
     }
-    public void showPillha(){
+    public void showPillha(){ // metodo que exibe a Pilha
         Node node = this.pilha.getTopo();
         do {
             Elemento elemento = node.getElemento();
-            System.out.println("ID: " + elemento.getId() + " Descricao: " + elemento.getDescricao() + " Data Hora: " + elemento.getDataHora());
+            System.out.println("ID: " + elemento.getId() + " Descricao: " + elemento.getDescricao() + " Data-Hora: " + elemento.getDataHora());
             node = node.getAnterior();
         } while (node != null);
     }
 
-    public void deleteElemento(){
+    public void deleteElemento(){ // metodo que deleta o elemento no topo da Pilha
         Node node = this.pilha.getTopo();
+        Elemento elemento = node.getElemento();
+        System.out.println("Deletando o dado de ID: " + elemento.getId() + " Descricao: " + elemento.getDescricao() + " Data-Hora: " + elemento.getDataHora());
         pilha.setTopo(node.getAnterior());
         node.setAnterior(null);
     }
--- Pilha/UserInterface.java
+++ Pilha/UserInterface.java
@@ -1,30 +1,42 @@
 import java.util.Scanner;
 
 public class UserInterface {
-    private PilhaController controller;
+    final PilhaController controller;
 
     public UserInterface(PilhaController controller) {
         this.controller = controller;
     }
 
+    public boolean app(){ // metodo principal onde rodamos todo o sistema
+        showOptions();
+        return chooseOption(option(1,4));
+    }
     /*
      * 1. input
      * 2. delete
      * 3. mostrar
      * 4.sair
      * */
-    public boolean chooseOption(int option){
-        showOptions();
+    public boolean chooseOption(int option){ // Metodo com o switch case onde os dados sao selecionados
         switch (option){
             case 1:
                 inserirDado();
                 return true;
+            case 2:
+                deleteDado();
+                return true;
+            case 3:
+                exibirHistorico();
+                return true;
+            case 4:
+                adeus();
+                return false;
             default:
                 return true;
         }
     }
 
-    public void inserirDado(){
+    public void inserirDado(){ // metodo que realiza a interacao com o usuario para inserir um novo elemento
         Scanner teclado = new Scanner(System.in);
         String id = "";
         String descricao = "";
@@ -38,6 +50,7 @@ public class UserInterface {
             System.out.print("Digite a data e hora no formato \"dd-mm-yyyy hh:mm:ss\" se possivl: ");
             dataHora = teclado.nextLine();
 
+
             Elemento elemento = new Elemento(id, descricao, dataHora);
             controller.createNode(elemento);
         }   catch (Exception e) {
@@ -46,7 +59,26 @@ public class UserInterface {
         }
     }
 
-    public void showOptions(){
+    public void deleteDado(){ // metodo que realiza a interacao com o usuario para deletar o elemento topo da fila
+        try {
+            System.out.println("Deletando o ultimo dado da fila...");
+            controller.deleteElemento();
+        }   catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Algo deu errado...");
+        }
+    }
+
+    public void exibirHistorico(){ // metodo que exibe os dados da pilha, em ordem do ultimo dado inserido ao primeiro
+        System.out.println("Exibindo o historico");
+        controller.showPillha();
+    }
+
+    public void adeus(){ // despedida e agradecimento
+        System.out.println("Obrigado por utilizar nosso aplicatvo!!");
+        System.out.println("Feito por: \n --- Luiz Mathias Rivabem Filho --- \n >>> Rafaela Vecchi <<< \n === Tiago de Brito Follador === ");
+    }
+    public void showOptions(){ // metodo que contem as opcoes a serem exibidas e escolhidas pelo usuario
         System.out.println("[1] Inserir um elemento do historico");
         System.out.println("[2] Retirar um elemento do historico");
         System.out.println("[3] Mostrar historico");
@@ -54,7 +86,14 @@ public class UserInterface {
         System.out.print(">>> ");
     }
 
-//    public int option(int min, int max){
-//
-//    }
+    public int option(int min, int max){ // metodo em que trava a selecao de opcoes para evitar saidas inesperadas
+        Scanner teclado = new Scanner(System.in);
+        int opcao = 2;
+        do {
+            String message = opcao < min || opcao > max ? "Tente novamente" : "Digite um valor"; // operador ternario message = (expressao booleana) ? (se sim) : (se nao)
+            System.out.println(message);
+            opcao = teclado.nextInt();
+        } while (opcao < min || opcao > max);
+        return opcao;
+    }
 }
