ae092fdd48aac5f60390049a61069d40eae55951
luizmathias <luizmathiasrivabem@gmail.com>
2024-09-13 20:50:31 -0300

    Trabalho versão Final

--- Fila/UserInterface.java
+++ Fila/UserInterface.java
@@ -8,18 +8,19 @@ public class UserInterface {
         this.controller = controller;
     }
 
-    public boolean app(){ // metodo principal onde rodamos todo o sistema
+    public boolean app() { // método principal onde rodamos todo o sistema
         showOptions();
-        return chooseOption(option(1,4));
+        return chooseOption(option(1, 4));
     }
+
     /*
      * 1. input
      * 2. delete
      * 3. mostrar
-     * 4.sair
-     * */
-    public boolean chooseOption(int option){ // Metodo com o switch case onde os dados sao selecionados
-        switch (option){
+     * 4. sair
+     */
+    public boolean chooseOption(int option) { // Método com o switch case onde os dados são selecionados
+        switch (option) {
             case 1:
                 inserirDado();
                 return true;
@@ -37,90 +38,91 @@ public class UserInterface {
         }
     }
 
-    public void inserirDado(){ // metodo que realiza a interacao com o usuario para inserir um novo elemento
+    public void inserirDado() { // método que realiza a interação com o usuário para inserir um novo elemento
         Scanner teclado = new Scanner(System.in);
-        String id;
-        String nome;
-        String motivo;
+        String id, nome, motivo;
 
         try {
             System.out.print("Digite o ID: ");
             id = teclado.nextLine();
-            System.out.print("Digite a Descricao: ");
+            System.out.print("Digite a Descrição: ");
             motivo = teclado.nextLine();
             System.out.print("Digite seu nome: ");
             nome = teclado.nextLine();
 
-
-            Elemento elemento = new Elemento( id, nome, motivo);
+            Elemento elemento = new Elemento(id, nome, motivo);
             controller.createNode(elemento);
-        }   catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             System.out.println("Algo deu errado...");
         }
     }
 
-    public void deleteDado(){ // metodo que realiza a interacao com o usuario para deletar o elemento topo da fila
+    public void deleteDado() { // método que realiza a interação com o usuário para deletar o elemento topo da fila
         Scanner teclado = new Scanner(System.in);
         try {
-            System.out.println("Digite o Id do dado a ser deletado: ");
+            System.out.print("Digite o Id do dado a ser deletado: ");
             controller.deleteElemento(teclado.next());
-        }   catch (Exception e) {
-
+        } catch (Exception e) {
             System.out.println("Algo deu errado...");
-            System.out.println("O Id escolhido não existe");;
-
+            System.out.println("O Id escolhido não existe.");
         }
     }
 
-    public void exibirHistorico(){ // metodo que exibe os dados da pilha, em ordem do ultimo dado inserido ao primeiro
+    public void exibirHistorico() { // método que exibe os dados da pilha, em ordem do último dado inserido ao primeiro
         Scanner teclado = new Scanner(System.in);
         System.out.println("Deseja exibir a \n [1] lista normal \n [2] lista invertida");
-        try{
+        try {
             int option = teclado.nextInt();
             switch (option) {
                 case 1:
-                    System.out.println("Exibindo o historico");
+                    System.out.println("Exibindo o histórico");
                     controller.showFila();
                     break;
                 case 2:
-                    System.out.println("Exibindo o historico inverso");
+                    System.out.println("Exibindo o histórico inverso");
                     controller.showFilaByBack();
                     break;
-
                 default:
-                    System.out.println("Algo deu errado...");
-                    System.out.println("Valor escolhido é invalido, por favor selecione os valores descritos!");
+                    System.out.println("Valor inválido, por favor selecione os valores descritos!");
                     break;
-
             }
-
-        } catch (Exception e){
-            System.out.println("Algo deu errado...");
-            System.out.println("Valor escolhido é invalido, por favor selecione os valores descritos!");
+        } catch (InputMismatchException e) {
+            System.out.println("Valor inválido, por favor selecione os valores descritos!");
         }
     }
 
-    public void adeus(){ // despedida e agradecimento
-        System.out.println("Obrigado por utilizar nosso aplicatvo!!");
+    public void adeus() { // despedida e agradecimento
+        System.out.println("Obrigado por utilizar nosso aplicativo!!");
         System.out.println("Feito por: \n --- Luiz Mathias Rivabem Filho --- \n >>> Rafaela Vecchi <<< \n === Tiago de Brito Follador === ");
     }
-    public void showOptions(){ // metodo que contem as opcoes a serem exibidas e escolhidas pelo usuario
-        System.out.println("\n[1] Inserir um elemento do historico");
-        System.out.println("\n[2] Retirar um elemento do historico");
-        System.out.println("\n[3] Mostrar historico");
+
+    public void showOptions() {
+        System.out.println("\n[1] Inserir um elemento do histórico");
+        System.out.println("\n[2] Retirar um elemento do histórico");
+        System.out.println("\n[3] Mostrar histórico");
         System.out.println("\n[4] Sair");
-        System.out.print("Insira sua opção: ");
     }
 
-    public int option(int min, int max){ // metodo em que trava a selecao de opcoes para evitar saidas inesperadas
+    private void barrer() {
+        System.out.print("\n=============================================================\n");
+    }
+
+    public int option(int min, int max) { // método que trava a seleção de opções para evitar saídas inesperadas
         Scanner teclado = new Scanner(System.in);
-        int opcao = 2;
+        int opcao = -1;
+
         do {
-            String message = opcao < min || opcao > max ? "Tente novamente" : "Digite um valor"; // operador ternario message = (expressao booleana) ? (se sim) : (se nao)
-            System.out.println(message);
-            opcao = teclado.nextInt();
+            try {
+                System.out.print("Digite uma opção: ");
+                opcao = teclado.nextInt();
+
+            } catch (Exception e) {
+                System.out.println("Digite um valor válido.");
+                teclado.next(); // Limpa a entrada inválida
+            }
         } while (opcao < min || opcao > max);
+
         return opcao;
     }
 }
